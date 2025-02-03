dd95c7bc2bf9a6054d991855c4b2accc06e3debd
fd2 <nu-fd2@users.noreply.github.com>
2025-02-03 20:58:33 +0100

    wrrrkinn

---
+++ push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:45:36 by oel-mado          #+#    #+#             */
+/*   Updated: 2025/02/03 20:17:00 by oel-mado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdbool.h>
+#include "libft/libft.h"
+#include "ft_printf/src/ft_printf.h"
+
+#endif //PUSH_SWAP_H
--- s_init.c
+++ s_init.c
@@ -1,18 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   s_init.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/03 15:45:31 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/02/03 19:36:08 by oel-mado         ###   ########.fr       */
+/*   Created: 2025/02/03 18:13:25 by oel-mado          #+#    #+#             */
+/*   Updated: 2025/02/03 20:20:15 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+void s_init(char **arr, t_list *a)
 {
-    
+	int i;
+	int j;
+	bool s;
+
+	i = 0;
+	j = 0;
+	while (arr[i])
+	{
+		while (arr[i][j])
+		{
+			s = ft_isdigit(arr[i][j]);
+			if (s != 0)
+			{
+				ft_printf();
+			}
+		}
+		i++;
+	}
 }

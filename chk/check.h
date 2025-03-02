7462651c56186cd20c6450c60cc90e53fab3b43c
fd2 <nu-fd2@users.noreply.github.com>
2025-03-02 08:51:13 +0000

    walo

--- chk/check.h
+++ chk/check.h
@@ -6,7 +6,7 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 18:42:19 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/02/28 21:03:53 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/03/02 08:38:27 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	is_error();
 bool    is_number(char *arg);
 int		is_it_good(char **arg);
 int		is_multi_arg(char *arg);
+bool    is_dup(long nb, char *arr);
 
 #endif //CHECK_H
---
+++ mvs/mvs.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mvs.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/02 08:41:00 by oel-mado          #+#    #+#             */
+/*   Updated: 2025/03/02 08:47:07 by oel-mado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MVS_H
+#define MVS_H
+
+#include "../main/push_swap.h"
+
+void pa(t_stack a, t_stack b);
+void pb(t_stack a, t_stack b);
+void ra(t_stack a, t_stack b);
+void rb(t_stack a, t_stack b);
+void rr(t_stack a, t_stack b);
+void rra(t_stack a, t_stack b);
+void rrb(t_stack a, t_stack b);
+void rrr(t_stack a, t_stack b);
+void sa(t_stack a, t_stack b);
+void sb(t_stack a, t_stack b);
+void ss(t_stack a, t_stack b);
+
+#endif //MVS_H

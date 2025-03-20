4e5ff43ff762e80e0f4ac7e909331c910b837237
fd2 <nu-fd2@users.noreply.github.com>
2025-03-20 09:54:37 +0000

    yayayayay

--- mvs/push.c
+++ mvs/push.c
@@ -6,7 +6,7 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 03:53:09 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/03/20 09:29:13 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/03/20 09:54:11 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,22 @@
 
 void	push(t_stack **from, t_stack **to, char c)
 {
-	t_stack	*A;
-	t_stack	*B;
-	t_stack	*An;
-	t_stack	*Bf;
-	t_stack	*Ap;
-	t_stack	*Bp;
-	t_stack	*Ah;
-	t_stack	*Bh;
-	t_stack	*Al;
-	t_stack	*Bl;
-	A = *from;
-	B = (*to)->prev;
-	An = (*from)->next;
-	Bf = *to;
-	Ap = (*from)->prev;
-	Bp = ((*to)->prev)->prev;
-	Ah = An;
-	Bh = A;
-	Al = B;
-	Bl = Bp;
-	An->prev = B;
-	B->next = NULL;
-	B->prev = Ap;
-	Ap->next = B;
-	A->next = Bf;
-	A->prev = Bp;
-	Bp->next = NULL;
-	Bf->prev = A;
-	An->is_head = 1;
-	Bf->is_head = 0;
-	*to = Bh;
-	*from = Ah;
+	t_stack	*nu_h_f;
+	t_stack	*nu_e_t;
+
+	nu_h_f = (*from)->next;
+	nu_e_t = ((*to)->prev)->prev;
+	nu_h_f->prev = (*to)->prev;
+	((*to)->prev)->next = NULL;
+	((*to)->prev)->prev = (*from)->prev;
+	(((*to)->prev)->prev)->next = (*to)->prev;
+	(*from)->next = *to;
+	(*from)->prev = nu_e_t;
+	nu_e_t->next = NULL;
+	(*to)->prev = *from;
+	nu_h_f->is_head = 1;
+	(*to)->is_head = 0;
+	*to = *from;
+	*from = nu_h_f;
 	ft_printf("p%c\n", c);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:45:31 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/03/20 09:16:20 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/03/20 09:40:12 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,38 +24,38 @@ int	main(int ac, char **av)
 	ac = size;
 	ft_printf("\033[1;32mA Before : \33[0m");
 	s_print(a);
-	s_print_rev(a);
+	// s_print_rev(a);
 	ft_printf("\033[1;32mB Before : \33[0m");
 	s_print(b);
-	s_print_rev(b);
+	// s_print_rev(b);
 	push(&a, &b, 'b');
 	ft_printf("\033[1;33mA After : \33[0m");
 	s_print(a);
-	s_print_rev(a);
+	// s_print_rev(a);
 	ft_printf("\033[1;33mB After : \33[0m");
 	s_print(b);
-	s_print_rev(b);
+	// s_print_rev(b);
 	push(&a, &b, 'b');
 	ft_printf("\033[1;33mA After : \33[0m");
 	s_print(a);
-	s_print_rev(a);
+	// s_print_rev(a);
 	ft_printf("\033[1;33mB After : \33[0m");
 	s_print(b);
-	s_print_rev(b);
+	// s_print_rev(b);
 	push(&a, &b, 'b');
 	ft_printf("\033[1;33mA After : \33[0m");
 	s_print(a);
-	s_print_rev(a);
+	// s_print_rev(a);
 	ft_printf("\033[1;33mB After : \33[0m");
 	s_print(b);
-	s_print_rev(b);
-	push(&a, &b, 'b');
+	// s_print_rev(b);
+	push(&b, &a, 'a');
 	ft_printf("\033[1;33mA After : \33[0m");
 	s_print(a);
-	s_print_rev(a);
+	// s_print_rev(a);
 	ft_printf("\033[1;33mB After : \33[0m");
 	s_print(b);
-	s_print_rev(b);
+	// s_print_rev(b);
 	s_free(a);
 	s_free(b);
 	ac--;

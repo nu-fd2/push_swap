faaa6aec8c8ef68f80b96951ff14b23366aa3e42
fd2 <nu-fd2@users.noreply.github.com>
2025-03-21 12:41:21 +0000

    b9at l algo

--- mvs/src/reverse_rotate.c
+++ mvs/src/reverse_rotate.c
@@ -6,30 +6,36 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 03:55:15 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/03/21 05:25:16 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/03/21 12:40:17 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-// t_stack	*last(t_stack **who)
-// {
-// 	while((*who)->next && (*who)->is_null)
-// 		*who = (*who)->next;
-// 	if ((*who)->is_null)
-// 		return ((*who)->prev);
-// 	if ((*who)->next == NULL)
-// 		return (*who);
-// }
-
 void	reverse_rotate(t_stack **who)
 {
+	t_stack	*fst;
+	t_stack	*lst;
 	t_stack	*tmp;
+	t_stack	*head;
 
-	tmp = (*who)->prev;
-	tmp->next = (*who);
-	(tmp->prev)->next = NULL;
-	*who = tmp;
-	(*who)->is_head = 1;
-	((*who)->next)->is_head = 0;
+	fst = *who;
+	lst = last(who);
+	tmp = (fst->prev)->prev;
+	head = lst;
+	if (lst->next == NULL)
+		(lst->prev)->next = NULL;
+	else
+	{
+		tmp = (fst->prev);
+		(lst->prev)->next = lst->next;
+		(lst->next)->prev = lst->prev;
+	}
+	fst->prev = lst;
+	lst->next = fst;
+	lst->prev = tmp;
+	tmp = lst;
+	fst->is_head = 0;
+	head->is_head = 1;
+	*who = head;
 }

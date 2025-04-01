5e04210ad3d30c3ab5ef2ae82e5454331a43f6a3
fd2 <nu-fd2@users.noreply.github.com>
2025-04-01 20:20:57 +0000

    ( .)(. )

--- bonus/ops/in_push.c
+++ bonus/ops/in_push.c
@@ -6,7 +6,7 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 10:55:56 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/04/01 16:20:03 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/04/01 20:18:20 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	in_push(t_stack **a, t_stack **b, char *op)
 	if (op[1] == 'a' && op[2] == '\n')
 	{
 		if (!((*a)->prev)->is_null)
-			is_error();
+			return ;
 		else
 			pa(a, b);
 	}
 	else if (op[1] == 'b' && op[2] == '\n')
 	{
 		if (!((*b)->prev)->is_null)
-			is_error();
+			return ;
 		else
 			pb(a, b);
 	}
